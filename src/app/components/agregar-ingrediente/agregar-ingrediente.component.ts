57d9acf58f330366485f41b637f60b6dd36292e7
fabio-ramirez <framirez@transcomahue.com.ar>
2023-07-23 03:08:55 -0300

    Se agregan estilos y manejar errores en Ingresar Ingrediente

--- src/app/components/agregar-ingrediente/agregar-ingrediente.component.ts
+++ src/app/components/agregar-ingrediente/agregar-ingrediente.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+
 import { IngredienteService } from '../../services/ingrediente.service';
 import { Ingrediente } from 'src/app/models/ingrediente';
 
@@ -17,6 +19,8 @@ export class AgregarIngredienteComponent implements OnInit {
   public comentario: string = '';
   public ingrediente: Ingrediente = new Ingrediente;
   public mostrarModal: boolean = false;
+  public mensajeToast: string = '';
+  public mostrarToastFlag: boolean = false;
 
   constructor(
     private ingredienteService: IngredienteService,
@@ -39,12 +43,34 @@ export class AgregarIngredienteComponent implements OnInit {
       estado: 'creado'
     }
     this.mostrarModal = true;
-    this.ingredienteService.agregarIngrediente(this.ingrediente).subscribe(ingrediente => {
 
-    })
+
+    // ...
+
+    this.ingredienteService.agregarIngrediente(this.ingrediente).subscribe(
+      (ingrediente) => {
+        // Respuesta exitosa (status 201)
+        // Aquí puedes mostrar un mensaje de éxito o realizar cualquier otra acción necesaria.
+      },
+      (error) => {
+        // Error en la solicitud (status 400 u otro error)
+        if (error instanceof HttpErrorResponse) {
+          // Aquí puedes mostrar un mensaje de error o realizar cualquier otra acción necesaria.
+          // Por ejemplo, mostrar el mensaje de error en un componente de toast.
+          this.mostrarToastFlag = true;
+          this.mensajeToast = "Error al agregar el ingrediente: " + error.error.message;
+          setTimeout(() => {
+            this.mostrarToastFlag = false;
+          }, 5000);
+        } else {
+          console.error("Error desconocido:", error);
+          // Aquí puedes manejar otros tipos de errores si es necesario.
+        }
+      }
+    );
   }
 
-  volverIngrediente() {
+  cancelar() {
     this.router.navigate(['/ingrediente']);
   }
 
